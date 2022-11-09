29185e786003457f389064a678d80956395ced14
woliver99 <oliver_giroux@hotmail.com>
2022-11-09 10:02:30 -0500

    Imported

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Maple Network</title>
+        <link rel="shortcut icon" href="files/MapleNetwork-Pfp.png" type="image/x-icon" />
+        <meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/style.css">
+        <script src="js/scripts.js"></script>
+    </head>
+    <body>
+        <header>
+
+        </header>
+        <center><h1>Welcome To Maple Network</h1>
+        <h2>Our Website Is Currently Still Under Construction Hopefully Il Have The Time To Make It Soon</h2></center>
+        <footer>
+
+        </footer>
+    </body>
+</html>
